58f5b8a03d313dc826e6d4ae6579bd2d5a814e3d
Austin Bergstrom <awbergs@gmail.com>
2014-11-04 22:23:01 -0500

    adding like logic for shots

--- src/plugin.js
+++ src/plugin.js
@@ -1,11 +1,17 @@
 (function($){
 
-	var _apiEndpoint = 'https://api.dribbble.com/v1/';
+	var _apiEndpoint = 'https://api.dribbble.com/v1';
 
 	var _ = function(){
 
 		var _call = function(opts){
 			opts.url = _apiEndpoint + opts.url + '?access_token=' + this.token;
+			opts.type = opts.type || 'GET';
+			var oldSuccess = opts.success || function(){};
+			opts.success = function(a,b,c){
+				console.log(arguments);
+				oldSuccess.call(this, arguments);
+			}
 			return $.ajax(opts);
 		}
 
@@ -15,15 +21,49 @@
 
 		this.getShot = function(shotId){
 			return _call.call(this, {
-				url: '/shots/' + shotId,
-				type: 'GET',
-				success: function(resp){
-					console.log(resp);
+				url: '/shots/' + shotId
+			});
+		}
+
+		this.getCurrentUser = function(){
+			return _call.call(this, {
+				url: '/user'
+			})
+		}
+
+		this.doesCurrentUserLikeShot = function(shotId){
+			var deferred = $.Deferred();
+
+			_call.call(this, {
+				url: '/shots/'+shotId+'/like',
+				success: function(){
+
+					deferred.resolve(true);
 				},
-				error: function(e){
-					console.log(e);
+				error: function(resp){
+					if(resp.status === 404){
+						deferred.resolve(false)
+
+					}
 				}
 			});
+
+			return deferred.promise();
+		}
+
+		this.likeShot = function(shotId){
+			return _call.call(this, {
+				url: '/shots/'+shotId+'/like',
+				type: 'POST'
+			});
+		}
+
+		this.unlikeShot = function(shotId){
+			return _call.call(this, {
+				url: '/shots/'+shotId+'/like',
+				type: 'DELETE'
+			});
+
 		}
 
 	}
