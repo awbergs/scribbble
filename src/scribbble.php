c3a3f39e8b0e58b79f643e39b1fbf08ce2518504
Austin Bergstrom <awbergs@gmail.com>
2014-11-17 00:39:00 -0500

    wp plugin file. license update

---
+++ src/scribbble.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Scribbble
+ * Plugin URI: https://github.com/awbergs/scribbble
+ * Description: A plugin for dribbble shots
+ * Version: 1.0.0
+ * Author: Blended Method, LLC
+ * Author URI: http://www.blendedmethod.com
+ * Copywright: 2014 Blended Method, LLC  
+ */
